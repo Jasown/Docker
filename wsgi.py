40a92efb1bf453457d4c6583a4e48cf038f3b451
Tuhglife <Tuhnkielife@gmail.com>
2022-10-12 18:45:32 +0200

    Test

--- wsgi.py
+++ wsgi.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return f"ID: {socket.gethostname()} Test"
+    return f"ID: {socket.gethostname()}"
 
 if __name__ == "__main__":
     app.run(debug=True)
